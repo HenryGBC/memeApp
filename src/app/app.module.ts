92f8bffb78f43bea3074aeea89ea67861a415d77
HenryGBC <henry.bravo89@gmail.com>
2018-07-30 06:34:38 -0500

    Init sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { provideRoutes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from './modules/auth/auth.component';
+
 
 @NgModule({
   declarations: [
