0af2f1e3b8c6beaaccd491bce941959e0d2068f7
HenryGBC <henry.bravo89@gmail.com>
2018-08-10 21:03:17 -0500

    Init admin styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './modules/auth/auth.component';
+import { AdminComponent } from './modules/admin/admin.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        loadChildren: './modules/home/home.module#HomeModule'
+      },
+    ]
+  },
   {
     path: 'auth',
     component: AuthComponent,
